537e74042723ce738519c18344f0dca12b7f2d95
htadepal <Hema03@ge>
2022-05-03 09:18:20 +0200

    Initial commit

---
+++ src/test/java/com/microservicesdemo/MicroservicesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicesdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
